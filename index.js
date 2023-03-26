c4a04c2dfdc87b43c5e988975fae46d6de48bcf4
Yehwan Baek <baekokok98@gmail.com>
2023-03-26 21:49:57 +1100

    fixed error and added search pokemon

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     const formSearchPokemon = document.querySelector("#search-pokemon");
-    const formSearchType = document.querySelector("#type-calculater");  
+    const formSearchType = document.querySelector("#type-calculater");
+
+    const inputPokemon = document.querySelector("#inputPokemon")
     const typeFrom = document.querySelector("#from");
     const typeTo = document.querySelector("#to");
 
     formSearchPokemon.addEventListener("submit", (e) => {
         e.preventDefault();
-        const pokemonName = formSearchPokemon.value;       
+        const pokemonName = inputPokemon.value;
         searchPokemon(pokemonName);
     })
 
+    
+
     formSearchType.addEventListener("submit", (e) => {
         e.preventDefault();
-        const inputType = formSearchType.value;
+        const inputType = inputType.value;
         searchType(inputType)
     })
 })
 
 function searchPokemon (pokemonName) {
+    let div = document.querySelector("#pokemon-name-result")
+    div.innerHTML = " ";
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
     .then( (res) => res.json() )
     .then( (data) => {
+        let ul = document.createElement("ul")
         let name = document.createElement("li")
         let id = document.createElement("li")
         let type = document.createElement("li")
@@ -30,22 +38,29 @@ function searchPokemon (pokemonName) {
         let img2 = document.createElement("img")
         img2.width = 200;
         img2.height = 200;
-        let div = document.querySelector("#pokemon-name-result")
+        
 
-        name.innerHTML = "Name : " + data.name;
+        name.innerHTML = "NAME : " + data.name;
         id.innerHTML = "ID : " + data.id;
         type.innerHTML = "Types : " + data.types.map(type => type.type.name);
         img1.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + data.id + ".png";
         img2.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + data.id + ".png";
 
-        const pokemonUl = document.querySelector("#pokemon");
+        div.appendChild(img1)
+        div.appendChild(img2)
 
-        div.insertBefore(img1, pokemonUl)
-        div.insertBefore(img2, pokemonUl)
-        pokemonUl.appendChild(name)
-        pokemonUl.appendChild(id)
-        pokemonUl.appendChild(type)
-        
+        ul.appendChild(name)
+        ul.appendChild(id)
+        ul.appendChild(type)
+
+        div.appendChild(ul)
+    })
+    .catch( (err) => {
+        console.log(err)
+        let p = document.createElement("p");
+        p.innerHTML = "Sorry, I think this is not Pokemon. I can not find it.";
+        let div = document.querySelector("#result");
+        div.appendChild(p)
     })
 }
 
