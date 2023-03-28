81975b0ec4d973fa338b520c051084c52d3a8c0a
Yehwan Baek <baekokok98@gmail.com>
2023-03-29 00:07:16 +1100

    create index2.js

---
+++ index2.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputPokemon = document.querySelector("#inputPokemon")
+    const formSearchPokemon = document.querySelector("#search-pokemon");
+
+    formSearchPokemon.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const pokemonName = inputPokemon.value;
+        loadComment(pokemonName)
+    })
+})
+
+function loadComment(pokemonName) {
+    let div = document.querySelector("#comment");
+    div.innerHTML = "";
+    
+    fetch("http://localhost:3000/comment")
+      .then((res) => res.json())
+      .then((comment) => {
+        let coUl = document.createElement("ul");
+        for (let i = 0; i < comment.length; i++) {
+          if (comment[i].name === pokemonName || comment[i].id_number === parseInt(pokemonName)) {
+            let coLi = document.createElement("li");
+            coLi.innerHTML = comment[i].commented;
+            coUl.appendChild(coLi);
+          }
+        }
+        div.appendChild(coUl);
+      })
+      .catch((err) => console.log(err));
+  }
